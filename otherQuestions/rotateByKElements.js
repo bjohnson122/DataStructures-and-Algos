64218a93d502a8e5de496f7245d0585ee4626f99
Breana <brecjohnson@gmail.com>
2023-05-07 23:09:41 -0400

    rotate k elements

---
+++ otherQuestions/rotateByKElements.js
@@ -0,0 +1,21 @@
+/*
+Write a function that rotates a list by k elements.
+ For example [1,2,3,4,5,6] rotated by two becomes [3,4,5,6,1,2]. 
+ Try solving this without creating a copy of the list. 
+ How many swap or move operations do you need?
+  */
+
+function rotateList(arr, k) {
+  // ESTABLISHING K
+  if (k % arr.length == 0) { // if K is COMPLETELY divisible by arr.length
+    return arr; // We simply return the array (because it would wrap onto itself)
+  } else if (k > arr.length) {  // if K > length of the array,
+    k = k % arr.length; // we just need the remainder
+  }
+  const endingArr = arr.slice(0, k); //create a copy of the values we NEED from the original arr [1,2]
+  const beginningArr = arr.slice(k); // [3, 4, 5, 6]
+
+  return [...beginningArr, ...endingArr]; // [3, 4, 5, 6, 1, 2] -->
+}
+
+console.log(rotateList([1, 2, 3, 4, 5, 6], 2)); // [3, 4, 5, 6, 1, 2]
