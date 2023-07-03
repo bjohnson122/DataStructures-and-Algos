494d15d2d50a1bbd54b426edd202433abdc8ae0a
Breana <brecjohnson@gmail.com>
2023-07-03 14:40:29 -0400

    updated prompt to twoSum prob

--- leetCode/arraysLC/twoSum.js
+++ leetCode/arraysLC/twoSum.js
@@ -1,4 +1,14 @@
 /*
+PROMPT:
+Given an array of integers nums and an integer target, 
+return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution,
+and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
 INPUT: an array, a target integer 
 OUTPUT: an array of TWO indices 
 ** mustn't use the same ele twice
