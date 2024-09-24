277a9f3348044606ada4276b6c75db951fcb17ff
Breana <brecjohnson@gmail.com>
2024-09-24 15:48:05 -0400

    updated solution to validAnagram

--- leetCode/arraysLC/validAnagram.js
+++ leetCode/arraysLC/validAnagram.js
@@ -20,3 +20,56 @@ var isAnagram = function (s, t) {
 
   return true;
 };
+
+// Alternate Solution
+function isAnagramAgain(s, t) {
+  if (s.length !== t.length) return false;
+
+  let sObj = {};
+  let tObj = {};
+
+  for (let i = 0; i < s.length; i++) {
+    if (!sObj[s[i]]) {
+      sObj[s[i]] = 1;
+    } else {
+      sObj[s[i]] += 1;
+    }
+
+    if (!tObj[t[i]]) {
+      tObj[t[i]] = 1;
+    } else {
+      tObj[t[i]] += 1;
+    }
+  }
+
+  for (let key in sObj) {
+    if (sObj[key] !== tObj[key]) return false;
+  }
+  return true;
+}
+
+// Alternate Solution w/ Ternary
+function isAnagramAgain(s, t) {
+  if (s.length !== t.length) return false;
+
+  let sObj = {};
+  let tObj = {};
+
+  for (let i = 0; i < s.length; i++) {
+    let currentLetterInS = s[i];
+    let currentLetterInT = t[i];
+
+    sObj[currentLetterInS]
+      ? (sObj[currentLetterInS] += 1)
+      : (sObj[currentLetterInS] = 1);
+
+    tObj[currentLetterInT]
+      ? (tObj[currentLetterInT] += 1)
+      : (tObj[currentLetterInT] = 1);
+  }
+
+  for (let key in sObj) {
+    if (sObj[key] !== tObj[key]) return false;
+  }
+  return true;
+}
